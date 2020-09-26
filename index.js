3462e5fc3e015c5869f144a94c82a41b1b0eca9b
zahra.mahmood98 <zahra.mahmood98@hotmail.co.uk>
2020-09-26 23:03:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var katzDeliLine = []
 
 function takeANumber(katzDeliLine, name) {
- katzDeliLine.push('name');
- console.log(`Welcome, ${name}. you are number ${katzDeliLine.length} in line`)
+  if (katzDeliLine.length === 0) {
+    console.log(`Welcome, ${name}. you are number 1 in line`)
+  } else {
+    katzDeliLine.push(name);
+    console.log(`Welcome, ${name}. you are number ${katzDeliLine.length} in line`)
+  }
 }

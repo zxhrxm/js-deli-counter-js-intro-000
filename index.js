7b4638f66d284d7ec4ca94e2946a64394f7f9fe4
zahra.mahmood98 <zahra.mahmood98@hotmail.co.uk>
2020-09-26 23:11:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function takeANumber(katzDeli, name) {
   katzDeli.push(name);
   return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
 }
+
+function nowServing(katzDeli) {
+  return katzDeli[0];
+  katzDeli.shift()
+}

d6975d8f88b2c19958076b867c11820dd5c33308
zahra.mahmood98 <zahra.mahmood98@hotmail.co.uk>
2020-09-26 23:16:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nowServing(katzDeli) {
   if (katzDeli.length === 0) {
     return "There is nobody waiting to be served!"
   } else {
-    return `Currently serving ${katzDeli[0]}.`;
+    console.log( `Currently serving ${katzDeli[0]}.`);
     katzDeli.shift(0)
   }
 }

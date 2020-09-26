d68021db1cfabbabcb868145de1859ab105bb145
zahra.mahmood98 <zahra.mahmood98@hotmail.co.uk>
2020-09-26 23:29:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,9 +17,10 @@ function currentLine(line) {
   if (line.length === 0) {
     return "The line is currently empty."
   } else {
-    var i=0;
-    while( i< line.length) {
-       return ("The line is currently:" + ` ${i+1}. ` + ` ${line[i]}`)
+    var nan = []
+    for( let i=0,  i< line.length, i++) {
+       nan.push(`${i+1}. ${line[i]}`)
     }
+    return `The line is currently: ${nan.join(", ")}`
   }
 }
